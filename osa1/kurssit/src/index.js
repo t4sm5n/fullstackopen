a737bdbb187a0a682c1872a02f5b963747d17abc
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-05-16 23:16:41 +0300

    Refactor kurssit app per tehtävä 1.4, add link to solution to README.md

--- osa1/kurssit/src/index.js
+++ osa1/kurssit/src/index.js
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom';
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
     
-    const osa1 = {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-    }
-    
-    const osa2 = {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-    }
-    const osa3 = {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-    }
+    const osat = [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14
+        }
+    ]
 
     const Otsikko = ( props ) => {
         return (
@@ -27,16 +28,16 @@ const App = () => {
     const Sisalto = ( props ) => {
         return (
             <div>
-                <Osa osa={ props.osa1 } tehtavia={ props.tehtavia1 }/>
-                <Osa osa={ props.osa2 } tehtavia={ props.tehtavia2 }/>
-                <Osa osa={ props.osa3 } tehtavia={ props.tehtavia3 }/>
+                <Osa osa={ props.osat[0].nimi } tehtavia={ props.osat[0].tehtavia }/>
+                <Osa osa={ props.osat[1].nimi } tehtavia={ props.osat[1].tehtavia }/>
+                <Osa osa={ props.osat[2].nimi } tehtavia={ props.osat[2].tehtavia }/>
             </div>
         )
     }
 
     const Yhteensa = ( props ) => {
         return (
-            <p>Yhteensä { props.yhteensa } tehtävää</p>
+            <p>Yhteensä { props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia } tehtävää</p>
         )
     }
 
@@ -49,8 +50,8 @@ const App = () => {
     return (
         <div>
             <Otsikko kurssi={ kurssi } />
-            <Sisalto osa1={ osa1.nimi } osa2={ osa2.nimi } osa3={ osa3.nimi } tehtavia1={ osa1.tehtavia } tehtavia2={ osa2.tehtavia } tehtavia3={ osa3.tehtavia } />
-            <Yhteensa yhteensa={ osa1.tehtavia + osa2.tehtavia + osa3.tehtavia } />
+            <Sisalto osat={ osat } />
+            <Yhteensa osat={ osat } />
         </div>
     )
 }
