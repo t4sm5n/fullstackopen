8e3392a2513ff1aa327de3392258e0dea4d4f75f
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-16 17:52:55 +0300

    4.2

--- osa4/blogilista-api/index.js
+++ osa4/blogilista-api/index.js
@@ -1,44 +1,32 @@
-const http = require( 'http' );
 const express = require( 'express' );
-const app = express();
 const bodyParser = require( 'body-parser' );
 const cors = require( 'cors' );
+const morgan = require( 'morgan' );
 const mongoose = require( 'mongoose' );
+const blogsRouter = require( './controllers/blogs' );
 
-const Blog = mongoose.model( 'Blog', {
-	title: String,
-	author: String,
-	url: String,
-	likes: Number
+const app = express();
+
+morgan.token( 'body', ( request, response ) => {
+	return JSON.stringify( request.body );
 } );
 
-module.exports = Blog;
+const mongoUrl = 'mongodb://********:*********@ds123852.mlab.com:23852/blogilista-database';
+mongoose
+	.connect( mongoUrl, { useNewUrlParser: true } )
+	.then( () => {
+		console.log( 'connected to database', mongoUrl );
+	} )
+	.catch( error => {
+		console.log( error );
+	} );
 
 app.use( cors() );
 app.use( bodyParser.json() );
+app.use( morgan( ':method :url :body :status :res[content-length] - :response-time ms' ) );
+app.use( '/api/blogs', blogsRouter );
 
-const mongoUrl = 'mongodb://********:*********@ds123852.mlab.com:23852/blogilista-database';
-mongoose.connect( mongoUrl, { useNewUrlParser: true } );
-
-app.get( '/api/blogs', ( request, response ) => {
-	Blog
-		.find({})
-		.then( blogs => {
-			response.json( blogs )
-		} );
-} );
-
-app.post( '/api/blogs', ( request, response ) => {
-	const blog = new Blog( request.body );
-
-	blog
-		.save()
-		.then( result => {
-			response.status( 201 ).json( result )
-		} );
-} );
-
-const PORT = 3003
+const PORT = 3003;
 app.listen( PORT, () => {
-	console.log( `Server running on port ${PORT}` )
+	console.log( `Server running on port ${PORT}` );
 } );
---
+++ osa4/blogilista-api/modules/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema( {
+	title: String,
+	author: String,
+	url: String,
+	likes: Number
+} );
+
+blogSchema.statics.format = function( blog ) {
+	return {
+		title: blog.title,
+		author: blog.author,
+		url: blog.url,
+		likes: blog.likes,
+		id: blog._id
+	};
+};
+
+const Blog = mongoose.model( 'Blog', blogSchema );
+
+module.exports = Blog;
