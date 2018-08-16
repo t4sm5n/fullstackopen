7f5190afcfb79b4c4decf1b471c3eaac1ee48a0e
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-16 18:39:25 +0300

    4.4

--- osa4/blogilista-api/utils/list_helper.js
+++ osa4/blogilista-api/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = ( blogs ) => {
 	return 1;
 };
 
+const totalLikes = ( blogs ) => {
+	const reducer = ( total, current ) => {
+		return total + current.likes;
+	};
+
+	return blogs.length === 0 ? 0 : blogs.reduce( reducer, 0 );
+};
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes
 };
