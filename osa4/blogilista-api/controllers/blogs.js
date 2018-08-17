fa63eac1790b888d61868193888eccf8efd7f847
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-18 01:14:32 +0300

    4.14

--- osa4/blogilista-api/controllers/blogs.js
+++ osa4/blogilista-api/controllers/blogs.js
@@ -42,4 +42,24 @@ blogsRouter.delete( '/:id', async ( request, response ) => {
 	}
 } );
 
+blogsRouter.put( '/:id', async ( request, response ) => {
+	try {
+		const body = request.body;
+
+		const blog = {
+			title: body.title,
+			author: body.author,
+			url: body.url,
+			likes: body.likes
+		};
+
+		const updatedBlog = await Blog.findByIdAndUpdate( request.params.id, blog, { new: true } );
+		response.json( Blog.format( updatedBlog ) );
+
+	} catch ( exception ) {
+		console.log( exception );
+		response.status( 400 ).send({ error: 'id not found' });
+	}
+} );
+
 module.exports = blogsRouter;
