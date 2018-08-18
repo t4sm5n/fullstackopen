38e3d880fdd35c290f7f561135ed1a1ca7409051
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-08-18 12:03:43 +0300

    4.19

--- osa4/blogilista-api/controllers/blogs.js
+++ osa4/blogilista-api/controllers/blogs.js
@@ -2,6 +2,16 @@ const blogsRouter = require( 'express' ).Router();
 const Blog = require( '../models/blog' );
 const User = require( '../models/user' );
 
+const jwt = require( 'jsonwebtoken' );
+
+const getTokenFrom = ( request ) => {
+	const authorization = request.get( 'authorization' );
+	if ( authorization && authorization.toLowerCase().startsWith( 'bearer' ) ) {
+		return authorization.substring(7);
+	}
+	return null;
+}
+
 blogsRouter.get( '/', async ( request, response ) => {
 	const blogs = await Blog
 		.find({})
@@ -10,14 +20,21 @@ blogsRouter.get( '/', async ( request, response ) => {
 } );
 
 blogsRouter.post( '/', async ( request, response ) => {
+	const body = request.body;
+
 	try {
-		const body = request.body;
+		const token = getTokenFrom( request );
+		const decodedToken = jwt.verify( token, process.env.SECRET );
+
+		if ( !token || !decodedToken.id ) {
+			return response.status( 401 ).json({ error: 'token missing or invalid' });
+		}
 
 		if ( !body.author || !body.url ) {
 			return response.status( 400 ).json({ error: 'author and url are required' });
 		}
 
-		const user = await User.findById( body.userId );
+		const user = await User.findById( decodedToken.id );
 
 		const blog = new Blog( {
 			title: body.title,
@@ -30,9 +47,6 @@ blogsRouter.post( '/', async ( request, response ) => {
 		const savedBlog = await blog.save();
 
 		user.blogs = user.blogs.concat( savedBlog._id );
-
-		console.log( user );
-
 		await user.save();
 
 		response.status( 201 ).json( Blog.format( savedBlog ) );
