350b498acc0b9659e9bde6bcef05787231a2c43c
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-11-04 13:15:27 +0200

    5.6

--- osa5/blogilista-ui/src/App.js
+++ osa5/blogilista-ui/src/App.js
@@ -33,7 +33,7 @@ class App extends React.Component {
 
 	componentDidMount() {
 		blogService.getAll().then( blogs =>
-			this.setState( { blogs } )
+			this.setState({ blogs })
 		);
 
 		const userJSON = window.localStorage.getItem('blogAppUser');
--- osa5/blogilista-ui/src/components/Blog.js
+++ osa5/blogilista-ui/src/components/Blog.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
-const Blog = ( { blog } ) => (
-	<div>
-		{blog.title} {blog.author}
-	</div>
-);
+class Blog extends React.Component {
+	state = {
+		blog: this.props.blog,
+		expanded: false
+	};
 
-export default Blog
+	toggleExpanded = () => {
+		this.setState({
+			expanded: !this.state.expanded
+		});
+	};
+
+	render() {
+		const blogStyle = {
+			paddingTop: 10,
+			paddingLeft: 2,
+			border: 'solid',
+			borderWidth: 1,
+			marginBottom: 5
+		};
+
+		return (
+			<div onClick={this.toggleExpanded} style={blogStyle}>
+				{this.state.expanded ?
+					<div>
+						{this.state.blog.title} {this.state.blog.author}
+						<div>
+							<div>
+								<a href={this.state.blog.url}>{this.state.blog.url}</a>
+							</div>
+							<div>
+								{this.state.blog.likes} likes
+								<button>like</button>
+							</div>
+							<div>
+								added by {this.state.blog.user.name}
+							</div>
+						</div>
+					</div> :
+					<div>
+						{this.state.blog.title} {this.state.blog.author}
+					</div>
+				}
+			</div>
+	)
+	}
+
+}
+
+export default Blog;
