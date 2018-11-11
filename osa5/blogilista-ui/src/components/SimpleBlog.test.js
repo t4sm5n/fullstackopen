404707bf88ea796153e7d56e65412c27fb7b6fee
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-11-11 22:30:29 +0200

    5.13

--- osa5/blogilista-ui/src/components/SimpleBlog.test.js
+++ osa5/blogilista-ui/src/components/SimpleBlog.test.js
@@ -2,19 +2,36 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SimpleBlog from './SimpleBlog';
 
+const blog = {
+	title: 'First class tests',
+	author: 'Robert C. Martin',
+	likes: 12
+};
+
 describe.only('<SimpleBlog />', () => {
 	it('should render the title, the author and the likes', function () {
-		const blog = {
-			title: 'First class tests',
-			author: 'Robert C. Martin',
-			likes: 12
-		};
-
 		const blogComponent = shallow(<SimpleBlog blog={blog} />);
 		const detailsDiv = blogComponent.find('.details');
 		const likesDiv = blogComponent.find('.likes');
 
 		expect(detailsDiv.text()).toContain(blog.title, blog.author);
 		expect(likesDiv.text()).toContain(blog.likes);
+	});
+
+	it('should call the event handler twice, when the button is clicked twice', function () {
+		const mockHandler = jest.fn();
+
+		const blogComponent = shallow(
+			<SimpleBlog
+				blog={blog}
+				onClick={mockHandler}
+			/>
+		);
+
+		const button = blogComponent.find('button');
+		button.simulate('click');
+		button.simulate('click');
+
+		expect(mockHandler.mock.calls.length).toBe(2);
 	});
 });
