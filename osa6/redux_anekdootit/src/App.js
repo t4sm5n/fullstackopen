a1c9afe2d3f150764101477cbbcf3949b8ce4293
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-11-18 21:14:07 +0200

    6.10

--- osa6/redux_anekdootit/src/App.js
+++ osa6/redux_anekdootit/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
 import Filter from './components/Filter';
+import anecdoteService from './services/anecdotes';
+import { anecdoteInitialization } from './reducers/anecdoteReducer';
 
 class App extends React.Component {
+	componentDidMount = async () => {
+		const notes = await anecdoteService.getAll();
+		this.props.anecdoteInitialization(notes);
+	};
 
 	render() {
 		return (
@@ -19,4 +27,7 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default connect(
+	null,
+	{ anecdoteInitialization }
+)(App);
--- osa6/redux_anekdootit/src/reducers/anecdoteReducer.js
+++ osa6/redux_anekdootit/src/reducers/anecdoteReducer.js
@@ -1,25 +1,6 @@
-const anecdotesAtStart = [
-	'If it hurts, do it more often',
-	'Adding manpower to a late software project makes it later!',
-	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-	'Premature optimization is the root of all evil.',
-	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
-	return {
-		content: anecdote,
-		id: getId(),
-		votes: 0
-	};
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const anecdoteReducer = (store = initialState, action) => {
+const anecdoteReducer = (store = [], action) => {
 	if (action.type === 'VOTE') {
 		const old = store.filter(a => a.id !== action.id);
 		const voted = store.find(a => a.id === action.id);
@@ -28,6 +9,9 @@ const anecdoteReducer = (store = initialState, action) => {
 	}
 	if (action.type === 'CREATE') {
 		return [...store, { content: action.content, id: getId(), votes: 0 }];
+	}
+	if (action.type === 'INIT_ANECDOTES') {
+		return action.data;
 	}
 
 	return store;
@@ -44,6 +28,13 @@ export const voting = (id) => {
 	return {
 		type: 'VOTE',
 		id
+	};
+};
+
+export const anecdoteInitialization = (data) => {
+	return {
+		type: 'INIT_ANECDOTES',
+		data
 	};
 };
 
