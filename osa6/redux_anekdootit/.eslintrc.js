32d483a5cb59770fc75007ea02aa5c37a72f2f84
Tuomas Mäkinen <tuomas.makinen2@centria.fi>
2018-11-18 14:51:05 +0200

    6.1

---
+++ osa6/redux_anekdootit/.eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "jest/globals": true
+    },
+    "extends": [
+    	"eslint:recommended",
+	    "plugin:react/recommended",
+        "plugin:jest/recommended"
+    ],
+	"parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+	    "jest"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+	    "no-trailing-spaces": "error",
+	    "object-curly-spacing": [
+		    "error", "always"
+	    ],
+	    "arrow-spacing": [
+		    "error", { "before": true, "after": true }
+	    ],
+	    "no-console": 0,
+	    "react/prop-types": 0,
+	    "jest/no-disabled-tests": "warn",
+	    "jest/no-focused-tests": "error",
+	    "jest/no-identical-title": "error",
+	    "jest/prefer-to-have-length": "warn",
+	    "jest/valid-expect": "error"
+    }
+};
